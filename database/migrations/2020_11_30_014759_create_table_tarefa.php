3b8419dd89a1596eec7a17d51f1813dc71aa408a
RafaelDomingues-RD <rafael.dominguesbr@gmail.com>
2020-12-04 16:06:03 -0300

    23367-Migrar biblioteca routerbox_fincanceiro

---
+++ database/migrations/2020_11_30_014759_create_table_tarefa.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableTarefa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('categoria_id');
+
+            $table->string('nome');
+            $table->string('criador');
+            $table->string('usuarioAlocado');
+            $table->text('descricao');
+            $table->slug('slug');
+
+            $table->timestamps();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
